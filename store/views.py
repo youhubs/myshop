4bfacfc63fc905f6215665104b2697810264f12d
Hu Tiger <huxy99@yahoo.com>
2020-07-15 18:00:37 -0500

    query cart data

--- store/views.py
+++ store/views.py
@@ -8,7 +8,14 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
