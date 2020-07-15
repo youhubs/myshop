c43f10e3e39cdfc076f3d8aa00d5da22f5cac52a
Hu Tiger <huxy99@yahoo.com>
2020-07-15 15:22:19 -0500

    add data models and register

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
+from store.models import Customer, Order, OrderItem, Product, ShippingAddress
 from django.contrib import admin
 
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
+
+
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
