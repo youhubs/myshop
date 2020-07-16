c4912303dcecec4652683c6745b8e8926369735e
Hu Tiger <huxy99@yahoo.com>
2020-07-15 21:52:28 -0500

    add updateUserOrder js function

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i = 0; i < updateBtns.length; i++) {
+for (i=0; i<updateBtns.length; i++){
 	updateBtns[i].addEventListener('click', function(){
 		var productId = this.dataset.product
 		var action = this.dataset.action
@@ -10,8 +10,29 @@ for (i = 0; i < updateBtns.length; i++) {
 		if (user == 'AnonymousUser'){
 			console.log('User is not authenticated.')
 		} else {
-			console.log('User is authenticated, sending data...')
+			updateUserOrder(productId, action)
 		}
+	})
+}
 
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+	var url = '/update_item/'
+	
+	fetch(url, {
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json'
+		},
+		body:JSON.stringify({'productID':productId, 'action':action})
+	})
+
+	.then((response) =>{
+		return response.json()
+	})
+
+	.then((data) =>{
+		console.log('data', data)
 	})
 }
