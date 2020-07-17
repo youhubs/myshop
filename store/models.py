76d2766ad6bd702c6da74fee9fa1e1bec22b36e9
Hu Tiger <huxy99@yahoo.com>
2020-07-16 21:39:59 -0500

    altered Product model field: DecimalField

--- store/models.py
+++ store/models.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
